39be9391c2977db3c4573de5a7eaa2aa32032a32
Jhonatan <jvasconcelosbittencourt@gmail.com>
2025-08-22 09:06:04 -0300

    ultimas atualizacoes

--- app-conversor-medidas/App.js
+++ app-conversor-medidas/App.js
@@ -33,7 +33,7 @@ export default function App() {
 
         <View style={styles.bannerContainer}>
           <BannerAd
-            unitId={TestIds.BANNER}
+            unitId="ca-app-pub-1408168476808382/3847637207"
             size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
             requestOptions={{
               requestNonPersonalizedAdsOnly: true,
